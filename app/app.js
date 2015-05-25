3afd45e5d24f11853bc1fcaca58a66e8a1f21cca
bernie macias <berniem@zillow.com>
2015-05-25 16:08:32 -0700

    move files from module 5

---
+++ app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+var express = require('express');
+var _ = require('underscore')
+var bodyParser = require('body-parser')
+var truckRoutes= require('./routes/truckRoutes');
+var app = express();
+var webport = process.env.PORT || 80;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/trucks', truckRoutes);
+
+app.listen(webport, function () {
+  console.log('web server started on port ' + webport);
+});
