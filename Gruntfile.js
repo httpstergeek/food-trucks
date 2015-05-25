80982cb9fb373468de5bf157f12cc2f8914e2c7b
bernie macias <berniem@zillow.com>
2015-05-25 16:29:08 -0700

    add basic grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,26 @@
 /**
  * Created by berniem on 5/25/15.
  */
+module.exports = function (grunt) {
+  //load plugins
+  [
+    'grunt-cafe-mocha',
+    'grunt-contrib-jshint',
+  ].forEach(function(task){
+      grunt.loadNpmTasks(task);
+    });
+
+  // configure plugins
+  grunt.initConfig({
+    cafemocha: {
+      all: { src: 'test/tests-*.js', options: {ui: 'tdd'},}
+    },
+    jshint: {
+      app: ['app.js', 'public/js/**/*.js', 'lib/**/*.js'],
+      qa: ['Gruntfile.js', 'public/test/**/*.js', 'test/**/*.js'],
+    },
+  });
+
+  // register task
+  grunt.registerTask('default', ['cafemocha', 'jshint']);
+};
